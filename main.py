3cacc8f4a4d091f4eb78348d3fc779a34a23bd16
vishwa <saivishwa.gaddam.com>
2023-07-26 22:36:01 -0700

    base model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 STAGE_NAME ="Data Ingestion Stage"
 
 if __name__ == '__main__':
@@ -11,3 +13,16 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e        
+    
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
