8bd85d09dcab4b04548110bbd10b6d06a6c28d44
vishwa <saivishwa.gaddam.com>
2023-07-26 18:21:52 -0700

    Data ingestion completed

---
+++ main.py
@@ -0,0 +1,13 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME ="Data Ingestion Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e        
